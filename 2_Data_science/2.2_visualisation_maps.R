ebf1719fff599a510ec320ab25596045b838dea4
AlbyDR <albyduarte@gmail.com>
2021-03-22 18:17:53 +0100

    viz G

--- 2_Data_science/2.2_visualisation_maps.R
+++ 2_Data_science/2.2_visualisation_maps.R
@@ -24,7 +24,7 @@ library(raster)
 library(RColorBrewer)
 library(geobuffer) }) # geobuffer_pts
 #'
-#' functions to import maps from the atlas 
+#' **Functions to import maps from the atla** 
 get_X_Y_coordinates <- function(x) {
   sftype <- as.character(sf::st_geometry_type(x, by_geometry = FALSE))
   if(sftype == "POINT") {
@@ -64,53 +64,38 @@ sf_save <- function(z, fname) {
   
 }
 #'
-#' funtion to resample
-resample_to_footprint = function(r, footprint_rast) {
-  r_new = raster::crop(x=r, y=extent(footprint_rast))  # first crop 
-  r_new = raster::projectRaster(r_new, footprint_rast) # reproject
-  return(r_new)
-}
-
-#'
-#'  import the Imprevious map - ekd102
-###################################################################
-# 01.02 Impervious Soil Coverage (Sealing of Soil Surface) (Edition 2017)
-###################################################################
-#	The real use of the built-up areas, the existing green and open space,
-# the urban structure, the sealing and the types of surface. 
-# Spatial reference block / partial block map ISU5 (Information System
-# City and Environment) as of December 31, 2015.
-####################################################################
+#'  import the Impervious map - ekd102
+###########################################################################
+# 01.02 Impervious Soil Coverage (Sealing of Soil Surface) (Edition 2017) #
+###########################################################################
+# The real use of the built-up areas, the existing green and open space, #
+# the urban structure, the sealing and the types of surface.              #
+# Spatial reference block / partial block map ISU5 (Information System    #
+# City and Environment) as of December 31, 2015.                          #
+###########################################################################
 #'
 impervious_map <- sf_fisbroker("https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/sach_nutz2015_nutzsa")
 #'
 impervious_map <- impervious_map %>%
-  dplyr::mutate(RAUMID = stringr::str_sub(gml_id, 12, 19)) %>%
-  dplyr::select(gml_id, RAUMID, everything()) %>%
-  dplyr::arrange(RAUMID)
+  mutate(RAUMID = stringr::str_sub(gml_id, 12, 19)) %>%
+  select(gml_id, RAUMID, everything()) %>%
+  arrange(RAUMID)
 #'
-dplyr::glimpse(impervious_map)
+glimpse(impervious_map)
 #'
-#' sf_save(impervious_map, "impervious_map")
-#'
-summary(impervious_map)
-str(filter(impervious_map, FLALLE==160724)) #key TUCC
-#$ VG        : num 68.9 = Sealing, track ballast is considered sealed [% of area size]
-#$ VG_0      : num 68.9 = Sealing, track ballast is considered unsealed [% of area size]
-#$ PROBAU    : num 47.5 = Built-up sealed area [% of area size]
-#$ PROVGNEU  : num 21.4 = Sealed area undeveloped, track ballast is considered sealed [% of area size]
-#$ PROVGNEU_0: num 21.4 = Unbuilt sealed area, track ballast is considered unsealed [% of area size]
-#$ KL1       : int 15   = Surface type 1 [% of undeveloped sealed area]
-#$ KL2       : int 70   = Surface type 2 [% of undeveloped sealed area]
-#$ KL3       : int 12   = Surface type 3 [% of undeveloped sealed area]
-#$ KL4       : int 3    = Surface type 4 [% of undeveloped sealed area]
+#' VG          Sealing, track ballast is considered sealed (% of area size)
+#' VG_0        Sealing, track ballast is considered unsealed (% of area size)
+#' PROBAU      Built-up sealed area (% of area size)
+#' PROVGNEU    Sealed area undeveloped, track ballast is considered sealed (% of area size)
+#' PROVGNEU_0  Unbuilt sealed area, track ballast is considered unsealed (% of area size)
 #'
 impervious_map <- st_transform(impervious_map, crs("+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs"))
 #'
-#' create an empty raster of 10m resolution
+#' Create an empty raster of 10m resolution
 map_raster <- raster(xmn=370000,xmx=415741,ymn=5799519,ymx=5837199, 
                res=10,crs=crs("+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs"))
-#'
+#' 
+#' Rasterize the polygons
 raster_imprevious <- raster::stack(
   fasterize(impervious_map, map_raster, field="VG",fun="max"),
   fasterize(impervious_map, map_raster, field="VG",fun="max"),
@@ -126,15 +111,13 @@ plot(raster_imprevious[[1]], col=terrain.colors(10))
 #'
 imprevious_zoom <- crop(raster_imprevious, extent(381673,388803,5811189,5821467))
 #'
-#'
 #' EC Tower and DWD stations points 
 points <- data.frame(
-  x = c(385566.5, 384597.4, 386525.1,385368.3),
-  y = c(5813229, 5812858, 5819332,5825159),
+  x = c(385566.5, 384597.4, 386525.1, 385368.3),
+  y = c(5813229, 5812858, 5819332, 5825159),
   city = c("ROTH", "DWD_Dahlem", "TUCC", "DWD_Tegel"))
 #'
-#'
-#' create a buffer around the tower
+#' Create a buffer around the tower
 pts_buf_1500m <- geobuffer_pts(xy = data.frame(lon = 13.32785,  
                                                lat = 52.51228), 
                                dist_m = 1500, output = "sp")
@@ -142,46 +125,44 @@ pts_buf_1500m <- geobuffer_pts(xy = data.frame(lon = 13.32785,
 summary(pts_buf_1500m)
 pts_buf_1500m <- spTransform(pts_buf_1500m, CRS("+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs"))
 #'
-plot(imprevious_zoom$Impervious,axes = F, box = F) #,horizontal=TRUE,legend.args=list(text='impervious (%)')
-plot(pts_buf_1500m, add = T, lwd = .5, fg = 2)  
-#'
 load(url("https://userpage.fu-berlin.de/soga/300/30100_data_sets/berlin_district.RData"))
 st_geometry(berlin.sf)
 berlin.Border <- st_transform(berlin.sf, crs(raster_imprevious))
 #'
-#' Vegetation Height Berlin
+#' Fraction of Unbuild areas
 ggplot() +
   layer_spatial(raster_imprevious$Unbuilt) + 
-  layer_spatial(berlin.sf, fill="transparent", size=1.2) + 
-  #layer_spatial(pts_buf_1500m, fill="transparent", col="red", size=1.2, linetype="dashed") + 
-  geom_spatial_point(aes(x=points$x[c(1,3)], y=points$y[c(1,3)]), 
-                     crs =crs(raster_imprevious), size=2, 
-                     col=c("black","black")) +
-  geom_spatial_label(aes(x=points$x[c(1,3)], y=points$y[c(1,3)],label=points$city[c(1,3)]),
+  layer_spatial(berlin.sf, fill = "transparent", size = 1.2) + 
+  geom_spatial_point(aes(x = points$x[c(1,3)], y = points$y[c(1,3)]),
+                     crs = crs(raster_imprevious), size = 2,
+                     col = c("black","black")) +
+  geom_spatial_label(aes(x = points$x[c(1,3)], y = points$y[c(1,3)],
+                         label = points$city[c(1,3)]),
                      crs = crs(raster_imprevious),
-                     fill="transparent", col=c("black","black"), 
-                     vjust=c(-0.5,-0.5),
-                     hjust=c(0.5, .05), label.size= 0, size=6) +
-  guides(linetype = guide_legend(title=NULL, order = 2), 
-         color = guide_legend(order=1)) +
-  scale_fill_gradientn(breaks=seq(0,30,5),limits=c(0,30),
-                       colors=brewer.pal('Greens', n=9), name="",na.value = NA,
-                       guide =guide_colorbar(direction = "horizontal",
+                     fill = "transparent", col = c("black","black"),
+                     vjust = c(-0.5, -0.5),
+                     hjust = c(0.5, 0.5), label.size = 0, size = 6) +
+  guides(linetype = guide_legend(title = NULL, order = 2), 
+         color = guide_legend(order = 1)) +
+  scale_fill_gradientn(breaks = seq(0, 30, 5),limits = c(0,30),
+                       colors = rev(brewer.pal('Greens', n = 9)), 
+                       name = " ", na.value = NA,
+                       guide = guide_colorbar(direction = "horizontal",
                                              label.position = "bottom",
-                                             title.vjust=9, label.vjust=9,
+                                             title.vjust = 9, label.vjust = 9,
                                              frame.colour = "black",
                                              frame.linewidth = 0.5,
                                              frame.linetype = 1,
                                              title.position = "left",
-                                             barwidth=30,barheight=1.2,nbin=30,
-                                             label.theme=element_text(angle=0,size=20))) +
+                                             barwidth = 25, barheight = 1.2, nbin = 30,
+                                             label.theme = element_text(angle = 0, size = 14))) +
   annotation_scale(location = "bl", height = unit(0.4, "cm"),
-                   pad_x=unit(1.75,"cm"), pad_y=unit(2.5,"cm"),
+                   pad_x = unit(1.5,"cm"), pad_y = unit(1.2,"cm"),
                    text_pad = unit(0.25, "cm"),
                    text_cex = 1.5) +
   annotation_north_arrow(location = "tr", which_north = "true",
-                         height = unit(2, "cm"),width = unit(2, "cm"),
-                         pad_x=unit(3.5,"cm"), pad_y=unit(2.5,"cm")) +
+                         height = unit(2, "cm"), width = unit(2, "cm"),
+                         pad_x=unit(1,"cm"), pad_y=unit(1.5,"cm")) +
   theme(axis.line=element_blank(),
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
@@ -194,32 +175,33 @@ ggplot() +
         panel.grid.major = element_blank(), 
         panel.background = element_rect("white"))
 #'
-#' impervious map
+#' Impervious map
 ggplot() +
   layer_spatial(imprevious_zoom$Impervious_with_str) + 
-  layer_spatial(pts_buf_1500m, fill="transparent", col="red", linetype="dashed", size=1.2) + 
-  geom_spatial_point(aes(x = points[3,1], y=points[3,2]), 
-                     crs =crs(imprevious_zoom), size = 2, 
-                     col=c("red")) +
-  geom_spatial_label(aes(x=points[3,1], y=points[3,2],
-                         label=points[3,3]),
-                     crs = crs(imprevious_zoom),
-                     fill="transparent", col=c("red"), 
-                     vjust=c(-0.5),
-                     hjust=c(0.3), label.size= 0, size=6) +
-  guides(linetype = guide_legend(title=NULL, order = 2), 
-         color = guide_legend(order=1)) +
-  scale_fill_gradientn(breaks=seq(0,100,10),limits=c(0,100),
-                       colors=terrain.colors(10), name="", na.value = 0,
-                       guide =guide_colorbar(direction = "horizontal",
+  layer_spatial(pts_buf_1500m, fill = "transparent", col = "red", 
+                linetype = "dashed", size = 1.2) + 
+  geom_spatial_point(aes(x = points$x[c(1,3)], y = points$y[c(1,3)]), 
+                     crs = crs(raster_imprevious), size = 2, 
+                     col = c("red","red")) +
+  geom_spatial_label(aes(x = points$x[c(1,3)], y = points$y[c(1,3)],
+                         label = points$city[c(1,3)]),
+                     crs = crs(raster_imprevious),
+                     fill = "transparent", col = c("red","red"), 
+                     vjust = c(-0.5,-0.5),
+                     hjust = c(0.5, .05), label.size = 0, size = 6) +
+  guides(linetype = guide_legend(title = NULL, order = 2), 
+         color = guide_legend(order = 1)) +
+  scale_fill_gradientn(breaks = seq(0,100,10), limits = c(0,100),
+                       colors = terrain.colors(10), name = "", na.value = 0,
+                       guide = guide_colorbar(direction = "horizontal",
                                              label.position = "bottom",
-                                             label.vjust=-2,
+                                             label.vjust = -2,
                                              frame.colour = "black",
                                              frame.linewidth = 0.5,
                                              frame.linetype = 1,
                                              #title.position = "left",
-                                             barwidth=21,barheight=1.2,nbin=10,
-                                             label.theme=element_text(angle=0,size=18))) +
+                                             barwidth=15, barheight=1.2, nbin=10,
+                                             label.theme=element_text(angle=0,size=14))) +
   theme(axis.line=element_blank(),
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
