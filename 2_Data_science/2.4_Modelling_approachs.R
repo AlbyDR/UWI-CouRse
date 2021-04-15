a7bbef6b3e9ed193b1550207f26960400251489a
AlbyDR <albyduarte@gmail.com>
2021-04-15 13:41:36 +0200

    modelling

--- 2_Data_science/2.4_Modelling_approachs.R
+++ 2_Data_science/2.4_Modelling_approachs.R
@@ -14,6 +14,10 @@ new.packages <- packages_list2[!(packages_list2 %in% installed.packages()[,"Pack
 
 if(length(new.packages)) install.packages(new.packages)
 
+update.packages <- packages_list2[(packages_list2 %in% old.packages()[,"Package"])]
+
+if(length(update.packages)) install.packages(update.packages)
+
 invisible(lapply(packages_list2, library, character.only = T, quietly = TRUE, warn.conflicts = F))
 #'
 #'
