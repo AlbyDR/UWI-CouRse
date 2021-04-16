e9c1dc7f9d5b60e68b12eada61035b365293d2a9
AlbyDR <albyduarte@gmail.com>
2021-04-16 10:59:14 +0200

    eva3

--- 5_Time-series/5.1-EVA_Extreme_Values.R
+++ 5_Time-series/5.1-EVA_Extreme_Values.R
@@ -10,11 +10,11 @@
 #'
 packages_list5.2 <- c("extRemes", "tidyverse", "lubridate")
 #'
-#' new.packages <- packages_list5.2[!(packages_list5.2 %in% installed.packages()[,"Package"])]
-#' if(length(new.packages)) install.packages(new.packages)
-#' #'
-#' update.packages <- packages_list5.2[(packages_list5.2 %in% old.packages()[,"Package"])]
-#' if(length(update.packages)) install.packages(update.packages)
+new.packages <- packages_list5.2[!(packages_list5.2 %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+#'
+update.packages <- packages_list5.2[(packages_list5.2 %in% old.packages()[,"Package"])]
+if(length(update.packages)) install.packages(update.packages)
 #'
 invisible(lapply(packages_list5.2, library, character.only = T, quietly = TRUE, warn.conflicts = F))
 #'
